6be5918e7cc49f3b96f8bb09a0951a380ef81286
Mateus Furquim <furquim.axs@gmail.com>
2022-12-20 01:22:58 -0300

    Score model and f1_score to file

--- scoring.py
+++ scoring.py
@@ -1,25 +1,45 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
+import pickle
+from pathlib import Path
 
-
+import pandas as pd
+from sklearn import metrics
 
 #################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open("config.json", "r") as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = Path(config["output_folder_path"])
+output_model_path = Path(config["output_model_path"])
+test_data_path = Path(config["test_data_path"])
+
+
+def preprocess_data():
+    test_filename = "testdata.csv"
+    X = pd.read_csv(Path(test_data_path, test_filename))
+    y = X.pop("exited")
+
+    X = X.select_dtypes(include=["number"])
+
+    return X, y
 
 
 #################Function for model scoring
-def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+def score_model(X, y):
+    # this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
+    # it should write the result to the latestscore.txt file
 
+    model_filename = "trainedmodel.pkl"
+    model = pickle.load(open(Path(output_model_path, model_filename), "rb"))
+    y_pred = model.predict(X)
+    f1 = metrics.f1_score(y, y_pred)
+
+    score_filename = "latestscore.txt"
+    score_path = Path(output_model_path, score_filename)
+    with open(score_path, "w") as fd:
+        fd.write(str(f1))
+
+
+if __name__ == "__main__":
+    X, y = preprocess_data()
+    score_model(X, y)
