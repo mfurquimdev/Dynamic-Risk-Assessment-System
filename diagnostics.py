68b434f6136163921dfe91835bfef3cf9504895c
Mateus Furquim <furquim.axs@gmail.com>
2022-12-20 02:24:44 -0300

    Add diagnostic script

--- diagnostics.py
+++ diagnostics.py
@@ -1,45 +1,107 @@
-
-import pandas as pd
-import numpy as np
-import timeit
-import os
 import json
+import os
+import pickle
+import subprocess
+import timeit
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
 
 ##################Load config.json and get environment variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+with open("config.json", "r") as f:
+    config = json.load(f)
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+
+dataset_csv_path = Path(config["output_folder_path"])
+test_data_path = Path(config["test_data_path"])
+prod_deployment_path = Path(config["prod_deployment_path"])
 
 ##################Function to get model predictions
-def model_predictions():
-    #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+def model_predictions(X):
+    # read the deployed model and a test dataset, calculate predictions
+    model_filename = "trainedmodel.pkl"
+    model = pickle.load(open(Path(prod_deployment_path, model_filename), "rb"))
+    y_pred = model.predict(X)
+
+    return y_pred  # return value should be a list containing all predictions
+
 
 ##################Function to get summary statistics
-def dataframe_summary():
-    #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+def dataframe_summary(X):
+    # calculate summary statistics here
+    summary_filename = "summary_statistics.csv"
+    summary_path = Path(dataset_csv_path, summary_filename)
+
+    summary_statistics = []
+    for col in X.columns:
+        mean = np.mean(X[col])
+        median = np.median(X[col])
+        stdev = np.std(X[col])
+        summary_statistics.append([mean, median, stdev])
+
+    X.describe().to_csv(summary_path)
+
+    return summary_statistics  # return value should be a list containing all summary statistics
+
+
+def check_missing_data(X):
+    nas = list(X.isna().sum())
+    na_percents = [nas[i] / len(X.index) for i in range(len(nas))]
+    return na_percents
+
 
 ##################Function to get timings
 def execution_time():
-    #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    # calculate timing of training.py and ingestion.py
+    execution_time_list = []
+
+    start_time = timeit.default_timer()
+    os.system("python3 ingestion.py")
+    execution_time_list.append(timeit.default_timer() - start_time)
+
+    start_time = timeit.default_timer()
+    os.system("python3 training.py")
+    execution_time_list.append(timeit.default_timer() - start_time)
+
+    return execution_time_list  # return a list of 2 timing values in seconds
+
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    # get a list of
+    broken = subprocess.check_output(["pip", "check"])
+    with open("broken.txt", "wb") as fd:
+        fd.write(broken)
+
+    installed = subprocess.check_output(["pip", "list"])
+    with open("installed.txt", "wb") as fd:
+        fd.write(installed)
+
+    requirements = subprocess.check_output(["pip", "freeze"])
+    with open("new_requirements.txt", "wb") as fd:
+        fd.write(requirements)
 
 
-if __name__ == '__main__':
-    model_predictions()
-    dataframe_summary()
-    execution_time()
+def preprocess_data():
+    test_filename = "testdata.csv"
+    X = pd.read_csv(Path(test_data_path, test_filename))
+    y = X.pop("exited")
+
+    X = X.select_dtypes(include=["number"])
+
+    return X, y
+
+
+if __name__ == "__main__":
+    X, y = preprocess_data()
+
+    y_pred = model_predictions(X)
+
+    summary = dataframe_summary(X)
+
+    check_missing_data(X)
+
+    dataframe_summary(X)
+    print(execution_time())
     outdated_packages_list()
-
-
-
-
-
-    
